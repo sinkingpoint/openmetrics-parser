8233e5ce566bd06011a6f00b017e830c1f6ddc16
sinkingpoint <colin@quirl.co.nz>
2023-05-15 09:37:55 +1000

    Allow empty metric families in Prometheus exposition
    This adds in a test for gravelgateway#17, which was a bug where a metric family
    with a descriptor, but without any metrics, would cause an invalid parse.
    
    To fix this, we update the parser to allow for empty metric families, and
    update the tests to ensure that we can parse the empty metric family.
    
    Signed-off-by: sinkingpoint <colin@quirl.co.nz>

--- src/prometheus/tests.rs
+++ src/prometheus/tests.rs
@@ -1,11 +1,16 @@
-use std::{fs, path::Path};
+use std::fs;
 
 use super::parsers::parse_prometheus;
 
 #[test]
 fn test_prometheus_parser() {
-    let child_str =
-        fs::read_to_string(Path::new("./src/prometheus/testdata/upstream_example.txt")).unwrap();
-
-    parse_prometheus(&child_str).unwrap();
+    for file in fs::read_dir("./src/prometheus/testdata").unwrap() {
+        let file = file.unwrap();
+        let path = file.path();
+        if path.extension().unwrap() == "txt" {
+            let child_str = fs::read_to_string(&path).unwrap();
+            let result = parse_prometheus(&child_str);
+            assert!(result.is_ok(), "failed to parse {}: {}", path.display(), result.err().unwrap());
+        }
+    }
 }
